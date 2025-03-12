7671ee8b96aa9147a5db390bebcbc92837d6a02a
Spuranthi Srirangam <6039411@mborijnland.nl>
2025-03-12 22:54:16 +0100

    home pagina & footer
    Voor de meeste gedeelte homepagina gedaan, maar moet nog een paar imperfecties vragen aan een docent. :)

---
+++ footer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <meta name="description" content="Umami Sushi, sushi restaurant">
+    <meta name="keywords" content="Umami Sushi, Umami, sushi, restaurant">
+    <meta name="author" content="Spuranthi Srirangam">
+
+    <title>Home pagina - Umami Sushi</title>
+    <link rel="icon" href="#" type="image/x-icon">
+    <link rel="stylesheet" href="stylesheet/umamisushi.css"> 
+</head>
+
+<body>
+    <footer class="footer">
+    </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="description" content="Welkom bij Umami Sushi! Hier vind je de beste en meest verrukkelijke sushi. Ons menu zit vol nieuwe smaken die je zeker niet teleurstellen. Alle informatie vind je op onze website. Kom langs en geniet! ">
+    <meta name="keywords" content="Umami Sushi, Umami, sushi, restaurant, ">
+    <meta name="author" content="Spuranthi Srirangam">
+
+    <title>Home pagina - Umami Sushi</title>
+    <link rel="icon" href="#" type="image/x-icon">
+    <link rel="stylesheet" href="stylesheet/umamisushi.css">
+</head>
+<body>
+    <header> 
+        <?php include('header.php');?>
+    </header>
+
+    <main>
+        <section>
+            <article class="banner">
+                <img src="plaatjes/sushi4.jpg" alt="sushi banner">
+                <p class="intro">Welkom bij Umami Sushi!<br> Hier vind je de beste en meest verrukkelijke sushi.<br> Ons menu zit vol nieuwe smaken die je zeker niet teleurstellen.<br> Alle informatie vind je op onze website. <br>Kom langs en geniet!</p>
+            </article>
+
+            <article class="home_tekst">
+                <p class="home_paragraaf">Umami Sushi is dé plek voor liefhebbers van authentieke en verse sushi.<br>Wij werken met de beste ingrediënten om jou een unieke smaakbeleving te bieden met een ruime keuze aan sushi, sashimi, poké bowls en andere Japanse specialiteiten!<br>
+                Wij bereiden alles dagelijks vers voor de beste kwaliteit met een uitgebreid menu vol klassieke sushi en verrassende creaties in een gezellige en moderne sfeer, perfect voor een avondje uit of om af te halen.<br>
+                Bestel eenvoudig online via onze website en geniet van de smaak van echte umami! :) </p>
+                <img src="plaatjes/sushi1.jpg" alt="foto van een assortiment sushi">
+            </article>
+        </section>
+    </main>
+
+    <footer>
+    <?php include('footer.php');?>
+    </footer>
+</body>
+</html>
