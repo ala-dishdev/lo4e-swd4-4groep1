984efc84a648d67f76f7fd3f051e020aaf444732
Spuranthi Srirangam <6039411@mborijnland.nl>
2025-03-17 14:25:11 +0100

    openingstijden

--- openingstijden.php
+++ openingstijden.php
@@ -10,7 +10,7 @@
     <meta name="author" content="Spuranthi Srirangam">
 
     <title>Openingstijden & locatie - Umami Sushi</title>
-    <link rel="icon" href="plaatjes/logo_umami_wit.png" type="image/x-icon">
+    <link rel="icon" href="plaatjes/logo_umami_sushi_1.png" type="image/x-icon">
     <link rel="stylesheet" href="stylesheet/umamisushi.css">
 </head>
 <body>
@@ -19,6 +19,21 @@
     </header>
 
     <main>
+        <!-- <article id="googleMap" style="width:100%;height:400px;color:white;"></article>
+        <script>
+            function myMap() {
+            var mapProp= {
+            center:new google.maps.LatLng(51.508742,-0.120850),
+            zoom:5,
+            };
+            var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+            }
+        </script> -->
+        <h1 class="topic">Openingstijden & Locatie</h1>
+        <article class="openingstijden_map">
+            <p class="openingstijden_intro">hwlp</p>
+        </article>
+
     </main>
 
     <footer>
