189f23dfd8eb9e922053c6b4799308d11b56244b
Spuranthi Srirangam <6039411@mborijnland.nl>
2025-03-17 13:28:30 +0100

    openingstijden & locatie pagina

---
+++ openingstijden.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="description" content="Welkom bij Umami Sushi! Hier vind je de beste en meest verrukkelijke sushi. Ons menu zit vol nieuwe smaken die je zeker niet teleurstellen. Alle informatie vind je op onze website. Kom langs en geniet! ">
+    <meta name="keywords" content="Umami Sushi, Umami, sushi, restaurant, ">
+    <meta name="author" content="Spuranthi Srirangam">
+
+    <title>Openingstijden & locatie - Umami Sushi</title>
+    <link rel="icon" href="plaatjes/logo_umami_wit.png" type="image/x-icon">
+    <link rel="stylesheet" href="stylesheet/umamisushi.css">
+</head>
+<body>
+    <header> 
+        <?php include('header.php');?>
+    </header>
+
+    <main>
+    </main>
+
+    <footer>
+    <?php include('footer.php');?>
+    </footer>
+</body>
+</html>
