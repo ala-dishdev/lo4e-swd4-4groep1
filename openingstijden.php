c8cc2e5d5912050b17272cf6b91a91a527e65462
Spuranthi Srirangam <6039411@mborijnland.nl>
2025-03-18 22:59:58 +0100

    openingstijden & map aangemaakt
    Moet alleen nog de tijden van de dagen dat ze open zijn opschrijven en ze een betere vorm geven. Moet ook nog het menu maken.

--- openingstijden.php
+++ openingstijden.php
@@ -19,20 +19,41 @@
     </header>
 
     <main>
-        <!-- <article id="googleMap" style="width:100%;height:400px;color:white;"></article>
-        <script>
-            function myMap() {
-            var mapProp= {
-            center:new google.maps.LatLng(51.508742,-0.120850),
-            zoom:5,
-            };
-            var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-            }
-        </script> -->
+        
         <h1 class="topic">Openingstijden & Locatie</h1>
         <article class="openingstijden_map">
-            <p class="openingstijden_intro">hwlp</p>
+        <iframe class='maps'src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4895.081213097955!2d4.481166876205538!3d52.16086376275743!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5c6ee75920585%3A0xb2bf2814cb018432!2sGalgewater%2012%2C%202311%20VZ%20Leiden!5e0!3m2!1sen!2snl!4v1742328388384!5m2!1sen!2snl" style="border:0;border-radius:20px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <p class="openingstijden_intro">Adresgegevens Umami Sushi:<br>Galgewater 12<br>2311VZ Leiden </p>
         </article>
+
+        <section aria-labelledby="gerecht_titel">
+        <h1 class="gerecht_titel">Openingstijden:</h1>
+        <ul>
+            <li>
+                <figure class="figure_1">
+                    <img src="klok.png" alt="Klok icoon">
+                </figure>
+                <p class="yellow">Lorem ipsum dolor sit amet</p>
+            </li>
+            <li>
+                <figure class="figure_2">
+                    <img src="#" alt="Klok icoon">
+                </figure>
+                <p class="orange">Lorem ipsum dolor sit amet</p>
+            </li>
+            <li>
+                <figure class="figure_3" >
+                    <img src="#" alt="Klok icoon">
+                </figure>
+                <p class="red">Lorem ipsum dolor sit amet</p>
+            </li>
+        </ul>
+    </section>
+
+    <article class="openingstijden_map">
+        <h1 class="openingstijden_intro"> ݁Wilt uw contact nemen met ons?<br>˚₊‧꒰ა 🍙 ໒꒱ ‧₊˚</h1>
+        <p class="openingstijden_intro">Email: info@umamileiden.nl <br><br> Telefoonnummer: 088-2324456</p>
+    </article>
 
     </main>
 
