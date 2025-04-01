091afb3380de56889714582a2c6e63566e0c90c3
Spuranthi Srirangam <6039411@mborijnland.nl>
2025-04-01 22:24:43 +0200

    light / dark modus
    de light dark modus werkt nu

---
+++ JS/light_modus.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let button = document.getElementById("lightModeButton");
+    let mainElement = document.getElementById("Lunchdiner_main"); 
+
+    if (button && mainElement) {
+        button.addEventListener("click", () => {
+            mainElement.classList.toggle("light_mode"); 
+        });
+    } else {
+        console.error("Knop of hoofdsectie niet gevonden!");
+    }
+});
